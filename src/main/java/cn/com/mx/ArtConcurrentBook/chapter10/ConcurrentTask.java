497b8dd1f8e8762b519009f7a622f2ffc022a666
sleeeeeeeep <2827873682@qq.com>
2016-08-10 14:32:44 +0800

    Java特种兵

--- src/main/java/cn/com/mx/ArtConcurrentBook/chapter10/ConcurrentTask.java
+++ src/main/java/cn/com/mx/ArtConcurrentBook/chapter10/ConcurrentTask.java
@@ -22,18 +22,17 @@ public class ConcurrentTask {
                         return taskName;
                     }
                 };
-                //1.2��������
+                //1.2锟斤拷锟斤拷锟斤拷锟斤拷
                 FutureTask<String> futureTask = new FutureTask<String>(task);
                 future = taskCache.putIfAbsent(taskName, futureTask); //1.3
                 if (future == null) {
                     future = futureTask;
-                    futureTask.run(); //1.4ִ������
+                    futureTask.run(); //1.4执锟斤拷锟斤拷锟斤拷
                 }
             }
 
             try {
-                return future.get(); //1.5,2.2�߳��ڴ˵ȴ�����ִ�����
-            } catch (CancellationException e) {
+                return future.get(); //1.5,2.2锟竭筹拷锟节此等达拷锟斤拷锟斤拷执锟斤拷锟斤拷锟�            } catch (CancellationException e) {
                 taskCache.remove(taskName, future);
             }
         }
