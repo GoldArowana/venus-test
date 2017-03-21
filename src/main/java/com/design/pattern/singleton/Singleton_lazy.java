03bf0cd83264bc31412ce53bb79d8807a74b2f82
wangchangye <2827873682@qq.com>
2017-03-21 16:23:01 +0800

    add  syn.

--- src/main/java/com/design/pattern/singleton/Singleton_lazy.java
+++ src/main/java/com/design/pattern/singleton/Singleton_lazy.java
@@ -2,13 +2,13 @@ package com.design.pattern.singleton;
 
 /**
  * Created by wangchangye on 2017/3/21.
- *
+ *  线程不安全  加了synchronized 安全 效率低
  * @Author wangchangye
  */
 public class Singleton_lazy {
     private static Singleton_lazy singleton_lazy;
     private Singleton_lazy(){};
-    public static Singleton_lazy getInstance(){
+    public static synchronized Singleton_lazy getInstance(){
         if(singleton_lazy==null){
             singleton_lazy = new Singleton_lazy();
         }
