b50ea9f0aec03948cbd49e878335b0732d16d973
wangchangye <2827873682@qq.com>
2017-03-27 11:46:04 +0800

    add static innerclass

--- src/main/java/com/design/pattern/singleton/staticInnerclass_singleton.java
+++ src/main/java/com/design/pattern/singleton/staticInnerclass_singleton.java
@@ -3,10 +3,13 @@ package com.design.pattern.singleton;
 
 /**
  * Created by wangchangye on 2016/12/2.
+ *  避免县城同步问题，延迟加载，效率较高。
  * @Author wangchangye
  */
 public class staticInnerclass_singleton {
     private staticInnerclass_singleton singleton;
+
+    private staticInnerclass_singleton(){};
     private static class getInstance{
         private static final staticInnerclass_singleton INSTANCE = new staticInnerclass_singleton();
     }
